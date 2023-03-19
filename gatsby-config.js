0fe8a097aac2a3f989b37695f08eb75e3e882fcd
Soyeong Kim <43427380+kimsoyeong@users.noreply.github.com>
2023-03-20 00:51:30 +0900

    Install npm packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@
  */
 module.exports = {
   siteMetadata: {
+    siteUrl: `https://kimsoyeong.github.io/`,
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     language: `Korean`,
@@ -204,6 +205,13 @@ module.exports = {
             },
           },
         ],
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: '/' }],
       },
     },
     // {
